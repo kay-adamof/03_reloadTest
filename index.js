ffd8d2696084ea875a6f3de243468dfac14be6f1
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2021-12-05 17:33:41 +0900

    Add reload core

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const reload = require('reload')
+const http = require('http')
+
 const app = express()
 const port = 3000
+
+const server = http.createServer(app)
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 })
 
+reload(app).then((reloadReturned) => {
+  server.listen(port, () => {
+    console.log(`Web server listening on port ${port}`);
+  })
+}).catch((err) => {
+  console.error('Reload could not start, could not start server/sample.app', err);
+})
