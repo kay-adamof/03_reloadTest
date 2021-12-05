503ec801f591e7ade63968a3e5e65e594ed964a3
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2021-12-05 17:54:40 +0900

    Move all code to server.js from index.js
    Because 'npm start' start server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const reload = require('reload')
+const http = require('http')
+
+let app = express()
+let port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+let server = http.createServer(app)
+
+reload(app).then((reloadReturned) => {
+  server.listen(port, () => {
+    console.log(`Web server listening on port ${port}`);
+  })
+}).catch((err) => {
+  console.error('Reload could not start, could not start server/sample.app', err);
+})
