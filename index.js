d20255ed79e3b707da1db52f12c7333a0cb0d5ff
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2021-12-05 15:46:33 +0900

    Start simple server

---
+++ index.js
@@ -0,0 +1,8 @@
+// Make simple server with no hot reload
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello world')
+})
+app.listen(3000)
