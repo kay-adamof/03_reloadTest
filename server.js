2d5dad1e8c39b9cee8216edc2fb88dde031f7c29
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2021-12-06 09:57:27 +0900

    Add error Handling

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.set('port',process.env.PORT || 3000)
 app.get('/', (req, res) => {
   res.sendFile("index.html", {root: __dirname})
 })
-app.listen(3000)
 
-// let server = http.createServer(app)
 
-// reload(app).then((reloadReturned) => {
-//   server.listen(app.get('port'), () => {
-//     console.log(`port:${app.get('port')}`);
-//   })
-// }).catch()　
+let server = http.createServer(app)
+
+reload(app).then((reloadReturned) => {
+  server.listen(app.get('port'), () => {
+    console.log(`port:${app.get('port')}`);
+  })
+}).catch((err) => {
+  console.error('Reload could not start');
+})
